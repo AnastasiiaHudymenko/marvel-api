3844d152c3565087004552014601110c514f835c
AnastasiiaHudymenko <106466791+AnastasiiaHudymenko@users.noreply.github.com>
2023-02-21 12:24:00 +0200

    add start template

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
-export const App = () => {
+import AppHeader from '../components/AppHeader/AppHeader';
+import RandomChar from '../components/RandomChar/RandomChar';
+import CharList from '../components/CharList/CharList';
+import CharInfo from '../components/CharInfo/CharInfo';
+
+import decoration from '../resources/img/vision.png';
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="app">
+      <AppHeader />
+      <main>
+        <RandomChar />
+        <div className="char__content">
+          <CharList />
+          <CharInfo />
+        </div>
+        <img className="bg-decoration" src={decoration} alt="vision" />
+      </main>
     </div>
   );
 };
+
+export default App;
