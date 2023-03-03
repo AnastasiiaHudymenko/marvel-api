d0a5266499c31b39c70011c4b7aaf4713c9d8f07
AnastasiiaHudymenko <106466791+AnastasiiaHudymenko@users.noreply.github.com>
2023-03-03 09:12:26 +0200

    Update AppHeader.js

--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -1,22 +1,29 @@
-import { Outlet, NavLink } from 'react-router-dom';
+import { Outlet, NavLink, useLocation } from 'react-router-dom';
 
 import './appHeader.scss';
 
 export const AppHeader = () => {
+  const location = useLocation();
+  console.log(location);
+
   return (
     <>
       <header className="app__header">
         <h1 className="app__title">
-          <a href="facebook.com">
+          <NavLink to="/">
             <span>Marvel</span> information portal
-          </a>
+          </NavLink>
         </h1>
         <nav className="app__menu">
           <ul style={{ gap: 20 }}>
-            <NavLink className="navLink" to="/">
+            <NavLink className="navLink" to="/" end>
               Characters
             </NavLink>
-            <NavLink className="navLink" to="/comics">
+            <NavLink
+              className="navLink"
+              to="/comics"
+              state={{ from: location }}
+            >
               Comics
             </NavLink>
           </ul>
