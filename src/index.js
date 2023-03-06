7a7582873dbfb2bf20bfdf58c2336095d0fed474
AnastasiiaHudymenko <106466791+AnastasiiaHudymenko@users.noreply.github.com>
2023-03-06 09:30:11 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import './style/variables.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <BrowserRouter basename="/">
+  <BrowserRouter basename="/marvel-api">
     <App />
   </BrowserRouter>
   // </React.StrictMode>
